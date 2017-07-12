aae5255df34e46b8f17da9b750c0b45485435395
Jakception <gbeulaygue@gmail.com>
2017-07-13 00:54:51 +0200

    Ajout Views première partie

---
+++ ChoixResto/Controllers/AccueilController.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+using ChoixResto.Models;
+using ChoixResto.ViewModels;
+
+namespace ChoixResto.Controllers
+{
+    public class AccueilController : Controller
+    {
+        // GET: Accueil
+        public ActionResult Index()
+        {
+            // Afficher une vue :
+            //
+            // return View();
+            // return View("Index");
+            // return View("Bonjour");
+            // return View("~/Views/Test/Essai.cshtml");
+
+            // ViewData, dictionnaire fourre-tout où nous pouvons mettre des objets en les associant à des clés.
+            //
+            ViewData["message"] = "Bonjour depuis le contrôleur !";
+            ViewData["date"] = DateTime.Now;
+            ViewData["resto"] = new Resto { Nom = "Choucroute party", Telephone = "01 60 14 80 55" };
+
+            //dynamic resto = new Resto();
+            //resto.Nom = "Resto dynam-hic";
+            //resto.Adresse = "compile, mais ne fonctionnera pas";
+
+            // ViewBag, Objet de type dynamic
+            //
+            ViewBag.message = "ReBonjour depuis le contrôleur !";
+            ViewBag.date = DateTime.Now;
+            ViewBag.resto = new Resto { Nom = "Pizza party", Telephone = "01 60 14 55 80" };
+
+            //
+            // return View();
+            //
+
+            // En passant un objet
+            //
+            Resto r = new Resto { Nom = "La bonne fourchette", Telephone = "1234" };
+            return View(r);
+        }
+
+        public ActionResult IndexVM()
+        {
+            AccueilViewModel vm = new AccueilViewModel
+            {
+                Message = "Bonjour depuis le contrôleur !",
+                Date = DateTime.Now,
+                Resto = new Resto { Nom = "Poutine ville", Telephone = "123456" }
+            };
+            return View(vm);
+        }
+
+        public ActionResult DayOfWeek()
+        {
+            return View(DateTime.Now);
+        }
+
+        public ActionResult IndexRestoVM()
+        {
+            AccueilRestoViewModel rvm = new AccueilRestoViewModel
+            {
+                Message = "Bonjour depuis le <span style=\"color:red\">contrôleur</span>",
+                Date = DateTime.Now,
+                ListeDesRestos = new List<Resto>
+                {
+                    new Resto { Nom = "Resto pinambour", Telephone = "1234" },
+                    new Resto { Nom = "Resto tologie", Telephone = "1234" },
+                    new Resto { Nom = "Resto ride", Telephone = "5678" },
+                    new Resto { Nom = "Resto toro", Telephone = "555" },
+                }
+            };
+            return View(rvm);
+        }
+    }
+}
