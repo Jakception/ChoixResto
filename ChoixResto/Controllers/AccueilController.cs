ef6c0499989e106ca7ef1087928d4b3d2c13c175
Jakception <gbeulaygue@gmail.com>
2017-07-17 01:19:54 +0200

    Début Helpers

--- ChoixResto/Controllers/AccueilController.cs
+++ ChoixResto/Controllers/AccueilController.cs
@@ -79,5 +79,27 @@ namespace ChoixResto.Controllers
             };
             return View(rvm);
         }
+
+        public ActionResult SelectList()
+        {
+            List<Models.Resto> listeDesRestos = new List<Resto>
+            {
+                new Resto { Id = 1, Nom = "Resto pinambour", Telephone = "1234" },
+                new Resto { Id = 2, Nom = "Resto tologie", Telephone = "1234" },
+                new Resto { Id = 5, Nom = "Resto ride", Telephone = "5678" },
+                new Resto { Id = 9, Nom = "Resto toro", Telephone = "555" },
+            };
+
+            ViewBag.ListeDesRestos = new SelectList(listeDesRestos, "Id", "Nom");
+            // Pour préselectionner
+            // ViewBag.ListeDesRestos = new SelectList(listeDesRestos, "Id", "Nom", 5);
+            return View();
+        }
+
+        public ActionResult Formulaire()
+        {
+            
+            return View();
+        }
     }
 }
